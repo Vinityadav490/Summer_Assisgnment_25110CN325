d320304b16daa2b8b33904ee4251928082d2992e
Vinityadav490 <vinityv070809@gmail.com>
2026-06-30 13:33:12 +0530

    Add files via upload

---
+++ Day12/palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(string s) {
+    int i = 0, j = s.size() - 1;
+
+    while (i < j) {
+        if (s[i] != s[j])
+            return false;
+
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (isPalindrome(s))
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
