8d08b312767320441a82dba041d71a2d5f96473c
Vinityadav490 <vinityv070809@gmail.com>
2026-06-30 13:27:53 +0530

    Add files via upload

---
+++ Day11/fforsum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << "Sum: " << add(x, y);
+    return 0;
+}
